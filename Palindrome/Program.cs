217fa69693cc0978c9f3560de3fff74493af362e
Kevin Thomas <Kevin Thomas>
2017-05-20 14:27:09 +0100

    Initial

---
+++ Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindromes are Great!");
+
+            string s = "sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop";
+            List<string> st = new List<string>(); 
+
+            for(int i = 0; i < s.Length; i++ )
+            {
+                for (int p = 0; p <= s.Length - i; p ++)
+                {
+                    if (s.Substring(i, p).SequenceEqual(s.Substring(i, p).Reverse())) st.Add(s.Substring(i, p)); 
+                }
+
+            }
+
+            foreach (string str in st.OrderByDescending(e => e.Length).Take(3))
+                Console.WriteLine(str + " : with index " + s.IndexOf(str));
+            Console.ReadLine(); 
+
+        }
+    }
+}
